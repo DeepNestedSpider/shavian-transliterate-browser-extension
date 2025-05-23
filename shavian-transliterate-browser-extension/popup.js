fdb23a85caa1e86d8f7fb383dccdafda62b73ca4
Deep Nested Spider <deepnestedspider@proton.me>
2025-05-23 05:55:53 +0000

    Purge

--- shavian-transliterate-browser-extension/popup.js
+++
@@ -1,7 +0,0 @@
-var __commonJS = (cb, mod) => () => (mod || cb((mod = { exports: {} }).exports, mod), mod.exports);
-
-// src/popup.ts
-var popup_default = undefined;
-export {
-  popup_default as default
-};
